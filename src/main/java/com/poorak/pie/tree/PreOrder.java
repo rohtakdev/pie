cc711b64580cc651f561b23c3e481508122846ec
poorak <poorakk@gmail.com>
2019-11-16 15:03:33 -0800

    adding preorder iterative

--- src/main/java/com/poorak/pie/tree/PreOrder.java
+++ src/main/java/com/poorak/pie/tree/PreOrder.java
@@ -1,10 +1,32 @@
 package com.poorak.pie.tree;
 
+import java.util.Stack;
+
 public class PreOrder {
-    public void print(BinaryTree.Node<Integer> node){
+    public void print_r(BinaryTree.Node<Integer> node){
         if(node == null) return;
         System.out.println(node);
-        if(node.getLeft() != null) print(node.getLeft());
-        if(node.getRight() != null) print(node.getRight());
+        if(node.getLeft() != null) print_r(node.getLeft());
+        if(node.getRight() != null) print_r(node.getRight());
+    }
+
+    public void print(BinaryTree.Node<Integer> node){
+        if(node == null) return;
+        BinaryTree.Node<Integer> curr ;
+        final Stack<BinaryTree.Node> stack = new Stack<>();
+        stack.push(node);
+        while(stack.size() > 0){
+            curr = stack.pop();
+            System.out.println(curr);
+
+            if(curr.getRight() != null){
+                stack.push(curr.getRight());
+            }
+
+            if(curr.getLeft() != null) {
+                stack.push(curr.getLeft());
+            }
+
+        }
     }
 }
--- src/test/java/com/poorak/pie/tree/PreOrderTest.java
+++ src/test/java/com/poorak/pie/tree/PreOrderTest.java
@@ -3,14 +3,17 @@ package com.poorak.pie.tree;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class PreOrderTest {
     private PreOrder preOrder;
 
     @BeforeEach
     void setUp() {
         preOrder = new PreOrder();
+    }
+
+    @Test
+    void print_r() {
+        preOrder.print_r(TreeProvider.create6_6());
     }
 
     @Test
