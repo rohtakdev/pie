e933265fab6393ebb25c4f3d37d7e4568bc74857
poorak <poorakk@gmail.com>
2019-11-20 10:49:14 -0800

    binary search - take 2

--- src/main/java/com/poorak/pie/recursion/BinarySearch.java
+++ src/main/java/com/poorak/pie/recursion/BinarySearch.java
@@ -3,13 +3,14 @@ package com.poorak.pie.recursion;
 public class BinarySearch {
     public int find(int[] arr, int element){
         if(arr.length ==0) return -1;
-        if(arr[0] > arr[arr.length-1]) return -2;
-        return find(arr, 0, arr.length, element);
+        return find(arr, 0, arr.length-1, element);
     }
 
     private int find(int[] arr, int start, int end, int element) {
-        if(end < start) return -1;
-        int mid = (start+end)/2 ;
+        int range = end-start;
+        if(range < 0) return -1;
+        if(arr[start] > arr[end]) return -2;
+        int mid = (range)/2  + start;
         if(arr[mid] == element) return mid;
         if(element > arr[mid]) return find(arr, mid+1, end, element);
         else return find(arr, start, mid-1, element);
--- src/test/java/com/poorak/pie/recursion/BinarySearchTest.java
+++ src/test/java/com/poorak/pie/recursion/BinarySearchTest.java
@@ -16,6 +16,7 @@ class BinarySearchTest {
     @Test
     void find() {
         assertEquals(3, binarySearch.find(new int[]{10, 20, 30, 40, 50}, 40));
+        assertEquals(-1, binarySearch.find(new int[]{10, 500, 30, 40, 50}, 20));
         assertEquals(-2, binarySearch.find(new int[]{600, 20, 30, 40, 50}, 40));
         assertEquals(-1, binarySearch.find(new int[]{}, 40));
 
