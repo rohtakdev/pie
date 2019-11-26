deb04b8288a2cdbf6fb2f13f9d04bc4198f91b79
poorak <poorakk@gmail.com>
2019-11-26 10:08:32 -0800

    adding sample output

--- src/main/java/com/poorak/pie/recursion/StringCombination.java
+++ src/main/java/com/poorak/pie/recursion/StringCombination.java
@@ -1,19 +1,39 @@
 package com.poorak.pie.recursion;
 
 public class StringCombination {
-    public void combine(String str){
-        if(str != null || !str.isEmpty()){
+    public void combine(String str) {
+        if (str != null || !str.isEmpty()) {
             combine(str, 0, new StringBuilder());
         }
     }
 
+    /**
+     * Sample output
+     * <p>
+     * w
+     * wx
+     * wxy
+     * wxyz
+     * wxz
+     * wy
+     * wyz
+     * wz
+     * x
+     * xy
+     * xyz
+     * xz
+     * y
+     * yz
+     * z
+     * </p>
+     */
     private void combine(String str, int pos, StringBuilder out) {
-        for(int i=pos;i<str.length();++i){
+        for (int i = pos; i < str.length(); ++i) {
             out.append(str.charAt(i));
             System.out.println(out);
-            if(i<str.length())
-                combine(str, i+1, out);
-            out.setLength(out.length()-1);
+            if (i < str.length())
+                combine(str, i + 1, out);
+            out.setLength(out.length() - 1);
         }
     }
 }
--- src/main/java/com/poorak/pie/recursion/StringPermutation.java
+++ src/main/java/com/poorak/pie/recursion/StringPermutation.java
@@ -1,26 +1,37 @@
 package com.poorak.pie.recursion;
 
 public class StringPermutation {
-    public void print(String str){
-        if(str == null) return;
-        if(str.isEmpty()) return;
+    public void print(String str) {
+        if (str == null) return;
+        if (str.isEmpty()) return;
         boolean[] used = new boolean[str.length()];
         StringBuilder out = new StringBuilder();
         print(str, used, out);
     }
 
+    /**
+     * Sample output
+     * <p>
+     * hat
+     * hta
+     * aht
+     * ath
+     * tha
+     * tah
+     * </p>
+     */
     private void print(String str, boolean[] used, StringBuilder out) {
-       if(out.length() == str.length()) {
-           System.out.println(out);
-           return;
-       }
-       for(int i =0 ;i < str.length() ; ++i){
-           if(used[i]) continue;
-           out.append(str.charAt(i));
-           used[i] = true;
-           print(str, used, out);
-           used[i] = false;
-           out.setLength(out.length()-1);
-       }
+        if (out.length() == str.length()) {
+            System.out.println(out);
+            return;
+        }
+        for (int i = 0; i < str.length(); ++i) {
+            if (used[i]) continue;
+            out.append(str.charAt(i));
+            used[i] = true;
+            print(str, used, out);
+            used[i] = false;
+            out.setLength(out.length() - 1);
+        }
     }
 }
